4c27eff9fb6d72b93bb3604e4e0e04ed4442498e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-12-05 15:28:55 +0500

    Enhance AppContextProvider to manage user authentication, chat state, and theme settings; integrate axios for API requests.

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,16 +1,40 @@
 "use client";
 import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-  const [count, setCount] = useState(0); // Example state
+  const navigate = useNavigate();
+  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
-  return (
-    <AppContext.Provider value={{ count, setCount }}>
-      {children}
-    </AppContext.Provider>
-  );
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  const [loadingUser, setLoadingUser] = useState(true);
+
+  const value = {
+    navigate,
+    axios,
+    user,
+    setUser,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    theme,
+    setTheme,
+    loadingUser,
+    token,
+    setToken,
+    fetchUser,
+    fetchUserChats,
+    createNewChat,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => useContext(AppContext);
